ae606d856ce8742513b50f8b95e41228e7b44e73
morgane-du-tweize <morgane_rt@hotmail.com>
2023-08-05 15:49:49 +0200

    code is now protected against SQL injection

--- database/MainDatabase.java
+++ database/MainDatabase.java
@@ -4,13 +4,12 @@ import java.io.FileReader;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
-import java.sql.Statement;
+import java.sql.PreparedStatement;
 import java.util.Properties;
 
 public class MainDatabase {
 	public static void main(String[] args) throws Exception {
-		
-		// Etablir la connection
+	
 		Properties props = new Properties();
 		try (FileReader fis = new FileReader("conf.properties")){
 			props.load(fis);
@@ -23,24 +22,25 @@ public class MainDatabase {
 		try (Connection connection = DriverManager.getConnection (url, login, password )){
 			
 			while (true) {
-				// Saisir données utilisateur
 				System.out.println("Veuillez saisir votre nom d'utilisateur");
 				String typedLogin = scan.nextLine().trim();
 				System.out.println("Veuillez saisir votre mot de passe");
 				String typedPassword = scan.nextLine().trim();
 
-				String strSql = "SELECT * FROM users WHERE login = '" + typedLogin + "' AND password = '" + typedPassword + "' ;" ;
+				String strSql = "SELECT * FROM users WHERE login =? AND password =? ;" ;
 				
-				try (
-					Statement statement = connection.createStatement();
-					ResultSet resultSet = statement.executeQuery(strSql);	
-					) {
-					if (resultSet.next()) {
-						String userLogin = resultSet.getString("login");
-						System.out.println("Bienvenue " + userLogin + " vous êtes maintenant connecté");
-						break ;
+				try (PreparedStatement statement = connection.prepareStatement(strSql)) {
+					statement.setString(1, typedLogin);
+					statement.setString(2, typedPassword);
+					try (ResultSet resultSet = statement.executeQuery()){
+						if (resultSet.next()) {
+							String userLogin = resultSet.getString("login");
+							System.out.println("Bienvenue " + userLogin + " vous êtes maintenant connecté");
+							break ;
+						}
 					}
 				}
+
 				System.out.println("Erreur - veuillez essayer de vous connecter à nouveau");
 			}
 		}
